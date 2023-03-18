ba46f839049f75cc33a607193b18bcf5b625388f
clickgirish <clicknaksh@gmail.com>
2023-03-18 19:23:51 +0530

    Corrected spaces

--- Madlib2.py
+++ Madlib2.py
@@ -5,4 +5,4 @@ verb= input("please entr a verb ending with ed: ")
 animal=input("please enter an animal of your choice: ")
 #preposition=
 print("Your Madlib")
-print("The",adjective,noun,verb, "over the lazy white",animal)
+print("The", adjective, noun, verb, "over the lazy white", animal)
