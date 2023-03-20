4fb49dd292d9622edeffe6dc418dafa8f1b3f094
clickgirish <clicknaksh@gmail.com>
2023-03-20 17:33:31 +0530

    Blue spiral

---
+++ colored spiral.py
@@ -0,0 +1,13 @@
+#colored spiral.py
+import turtle
+pen=turtle.Pen()
+pen.speed(200)
+pen.color("blue")
+#for x in range(100):
+for x in range(1500):
+    pen.forward(x)
+    pen.left(91)
+    #pen.forward(x)
+    #pen.left(90)
+    
+    
